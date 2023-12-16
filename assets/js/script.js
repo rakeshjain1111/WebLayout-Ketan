be3bdf715ba1b1596ad1a181485fb29b703c2e42
rakeshjain1111 <rakeshkjain222@gmail.com>
2023-12-16 17:37:52 +0530

    preloader added

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,12 @@ function prev(){
 }
 
 setInterval(next,3000)
+
+
+
+// ====PRELOADER
+
+var loader = document.getElementById("preloader");
+window.addEventListener("load",()=>{
+    loader.style.display="none";
+});
